fe713e102c20d7abb4ad54663f2974537b086ef1
Alex Albu <alex.albu@bloomandwild.com>
2024-09-20 13:37:59 +0100

    add v0.3.0

--- bw-cli.rb
+++ bw-cli.rb
@@ -1,10 +1,10 @@
 class BwCli < Formula
     desc "CLI tool to manage ECS services and modify desired counts"
     homepage "https://github.com/alexalbu001/bw-cli"
-    url "https://github.com/alexalbu001/bw-cli/releases/download/v0.2.0/bw-cli.tar.gz"
-    sha256 "23269fa3d033de56d87871f6f4085f511ac3d0a1ea1f775fa658609f74dc187e"
+    url "https://github.com/alexalbu001/bw-cli/releases/download/v0.3.0/bw-cli.tar.gz"
+    sha256 "64eb815a9911c592749086e9540f21378a26bb6ad33c7779413f1b9c681b7936"
     license "MIT"
-    version "v0.2.0"
+    version "v0.3.0"
   
     def install
       bin.install "bw-cli"
